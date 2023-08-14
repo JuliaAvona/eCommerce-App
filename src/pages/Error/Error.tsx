11b7409a2185a06e5b869831d820be4ae151efa6
kseniaagapitova <kagapitova@inbox.ru>
2023-08-14 14:01:39 +0300

    feat: decorate error page

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,16 +1,20 @@
-import './Error.module.css';
+import styles from './Error.module.css';
+import React from "react";
 import {Nav} from "react-bootstrap";
 import {Pages} from "../../types/enums";
-import React from "react";
 
 const Error = () => {
   return (
-      <>
-        <div className="mt=20">Error Page 404</div>;
-        <Nav className="me-auto">
-          <Nav.Link href={Pages.main}>Home</Nav.Link>
-        </Nav>
-      </>
+        <div className="mt=20">
+            <img src="https://serviceholod.by/wp-content/uploads/2020/02/pustoy-holodilnik-mozhno-li-vklyuchat-1024x681.jpg" alt="holod"/>
+           <div className={styles.text1}>
+               <pre>Error Page 404</pre>
+               <pre>It's empty, try again later.</pre>
+               <div className="btn btn-dark">
+                   <Nav.Link href={Pages.main} className='m-2'>To home page.</Nav.Link>
+               </div>
+           </div>
+        </div>
       )
 };
 
