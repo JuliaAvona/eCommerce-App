61dcc1dfc5d69099140af0a348eac9b92fd1b1fe
ZEROM22 <gjxnfjnueukf220827@gmail.com>
2023-08-21 21:48:18 +0300

    fix: tag duplication fixed

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -6,14 +6,6 @@ const Main = () => {
   return (
     <div className="m-auto">
       <Image className={styles.plant} src={previewImg} roundedCircle />
-      <pre className={styles.text1}>
-        Welcome to the eco goods store. We help save the planet for future generations.
-      </pre>
-      <Image
-        className={styles.plant}
-        src="https://uokansk.ucoz.ru/document/news_2019/aprel/slet_liderov-rostok.jpg"
-        roundedCircle
-      />
       <pre className={styles.text1}>Welcome to the eco goods store.</pre>
       <pre className={styles.text1}>We help save the planet for future generations.</pre>
     </div>
