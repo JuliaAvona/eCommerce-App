5dc07cff86c4688a6788785b9326e6510d84d5f6
kseniaagapitova <kagapitova@inbox.ru>
2023-08-27 01:30:38 +0300

    feat: product img slider with no variants

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -2,18 +2,28 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import React from 'react';
 import 'swiper/css';
 import { Product } from '../../types/interfaces';
+import styles from './Slider.module.css';
+import 'swiper/css/pagination';
 
-const Slider = ({ name, variants }: Product) => {
-  const sliderElements = variants.map((el) => (
-    <SwiperSlide key={el.images[0].url}>
-      <img src={el.images[0].url} alt={name['en-US']} />
-    </SwiperSlide>
-  ));
+const Slider = ({ name, variants, masterVariant }: Product) => {
+  let sliderElements: JSX.Element[] = [
+    <SwiperSlide key={masterVariant.images[0].url}>
+      <img className={styles.img} src={masterVariant.images[0].url} alt={name['en-US']} />
+    </SwiperSlide>,
+  ];
+  if (variants.length !== 0) {
+    sliderElements = variants.map((el) => (
+      <SwiperSlide key={el.images[0].url}>
+        <img className={styles.img} src={el.images[0].url} alt={name['en-US']} />
+      </SwiperSlide>
+    ));
+  }
 
   return (
     <Swiper
-      spaceBetween={50}
+      spaceBetween={3}
       slidesPerView={1}
+      pagination={{ clickable: true }}
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
     >
